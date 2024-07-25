9e365a920a9c2fa050bee7f53ab0a8edf7e07b82
Cserbik Róbert Ármin <129281532+robotarmin@users.noreply.github.com>
2024-07-25 22:03:17 +0200

    tried to add a flashlight function but it all went south so finishing here for today

--- app/src/main/java/com/robotarmin/myapplication/MainActivity.java
+++ app/src/main/java/com/robotarmin/myapplication/MainActivity.java
@@ -2,8 +2,6 @@ package com.robotarmin.myapplication;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
