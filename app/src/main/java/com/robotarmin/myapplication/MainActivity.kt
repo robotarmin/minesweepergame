77da6db05602e103489037e18e2ac904e5a47fde
Cserbik Róbert Ármin <129281532+robotarmin@users.noreply.github.com>
2024-07-25 19:59:38 +0200

    first commit this is the full project of just a sample app and a greeting, im gonna build it and run it on my phone now

--- app/src/main/java/com/robotarmin/myapplication/MainActivity.kt
+++ app/src/main/java/com/robotarmin/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.robotarmin.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Red) {
         Text(
             text = "Hi $name! How are you feeling on this warm summer day?",
-            modifier = modifier
+            modifier = modifier.padding(69.dp)
         )
     }
 }
